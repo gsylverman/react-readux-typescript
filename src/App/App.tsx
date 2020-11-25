397cf833f54b5a472effafb0b09907d31fb5f318
gavril.hasmasan <gavril.hasmasan@pitechnologies.ro>
2020-11-25 15:34:00 +0200

    f

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { computeHeadingLevel } from "@testing-library/react";
 
 function App() {
   const [state, setstate] = useState<Array<{}>>([]);
